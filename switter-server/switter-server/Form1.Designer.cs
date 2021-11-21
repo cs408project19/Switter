0d57ff1ef520f439f267bd667c36d7bf13ef989f
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-21 16:10:56 +0300

    finished server

---
+++ switter-server/switter-server/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace switter_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbox_port = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.richtextbox_log = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Port:";
+            // 
+            // textbox_port
+            // 
+            this.textbox_port.Location = new System.Drawing.Point(82, 32);
+            this.textbox_port.Name = "textbox_port";
+            this.textbox_port.Size = new System.Drawing.Size(100, 20);
+            this.textbox_port.TabIndex = 1;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(203, 31);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // richtextbox_log
+            // 
+            this.richtextbox_log.Location = new System.Drawing.Point(16, 72);
+            this.richtextbox_log.Name = "richtextbox_log";
+            this.richtextbox_log.Size = new System.Drawing.Size(601, 300);
+            this.richtextbox_log.TabIndex = 3;
+            this.richtextbox_log.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 433);
+            this.Controls.Add(this.richtextbox_log);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textbox_port);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textbox_port;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.RichTextBox richtextbox_log;
+    }
+}
+
---
+++ switter-server/switter-server/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switter_server
+{
+    public partial class Form1 : Form
+    {
+        bool terminating = false;
+        bool listening = false;
+        List<string> users;
+        List<string> connectedUsers = new List<string>();
+        List<string> sweets = new List<string>();
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+        void readUserFile()
+        {
+            users = File.ReadAllLines("user-db.txt").ToList<string>();
+        }
+
+        void readSweetsFile()
+        {
+            sweets = File.ReadAllLines("sweets.txt").ToList<string>();
+        }
+
+        bool userExist(string username)
+        {
+            foreach(string u in users)
+            {
+                if (u == username)
+                    return true;
+            }
+            return false;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            readUserFile();
+            readSweetsFile();
+
+            int serverPort;
+
+            if (Int32.TryParse(textbox_port.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(3);
+
+                listening = true;
+                button_start.Enabled = false;
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                richtextbox_log.AppendText("Started listening on port: " + serverPort + "\n");
+
+            }
+            else
+            {
+                richtextbox_log.AppendText("Please check port number \n");
+            }
+        }
+
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+                    clientSockets.Add(newClient);
+
+                    Byte[] buffer = new Byte[128];
+                    newClient.Receive(buffer);
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+
+                    richtextbox_log.AppendText(incomingMessage + " is trying to connect\n");
+
+                    if (userExist(incomingMessage))
+                    {
+                        if (connectedUsers.Exists(x => x.Equals(incomingMessage))) {
+                            richtextbox_log.AppendText("User already connected!\n");
+                            newClient.Close();
+                            clientSockets.Remove(newClient);
+                        }
+                        else
+                        {
+                            connectedUsers.Add(incomingMessage);
+                            richtextbox_log.AppendText(incomingMessage + " connected!\n");
+                        }
+                    }
+                    else
+                    {
+                        richtextbox_log.AppendText("User does not exist!\n");
+                        newClient.Close();
+                        clientSockets.Remove(newClient);
+                    }
+
+                    Thread receiveThread = new Thread(() => Receive(newClient)); // updated
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        richtextbox_log.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
+        }
+
+        void AddSweet(string sweet)
+        {
+            sweets.Add(sweet);
+
+            File.WriteAllLines("sweets.txt", sweets);
+        }
+
+        private void Receive(Socket thisClient) // updated
+        {
+            bool connected = true;
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    if (incomingMessage == "request")
+                    {
+                        string allSweets = "";
+                        foreach(string sweet in sweets)
+                        {
+                            allSweets += sweet + "\n";
+                        }
+
+                        richtextbox_log.AppendText("A client requested all sweets.\n");
+
+                        Byte[] sweetsBuffer = Encoding.Default.GetBytes(allSweets);
+                        thisClient.Send(sweetsBuffer);
+                    }
+
+                    else
+                    {
+                        if(incomingMessage.Split(';').Length >= 2) { 
+                            string user = incomingMessage.Split(';')[0];
+                            string msg = incomingMessage.Split(';')[1];
+
+                            if (userExist(user))
+                            {
+                                string sweet = user + ";" + msg + ";" + DateTime.Now.ToUniversalTime().ToString() + ";" + (sweets.Count + 1).ToString();
+                                AddSweet(sweet);
+                                richtextbox_log.AppendText(sweet + "\n");
+                            }
+                            else
+                            {
+                                richtextbox_log.AppendText("Invalid username!\n");
+                            }
+                        }
+
+                        else
+                        {
+                            richtextbox_log.AppendText("Invalid message!\n");
+                        }
+                    }
+
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        richtextbox_log.AppendText("A client has disconnected\n");
+                    }
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                    connected = false;
+                }
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+    }
+}
